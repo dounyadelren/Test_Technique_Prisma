2f6b2cd94e08d9b0fe7fbc448c405790bf03fcb8
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-19 17:13:49 +0100

    [add] first commit, create react app

---
+++ starwars/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      'sm': {'max': '375px'},
+      'lg': {'min': '376px', 'max': '1024px'},
+    },
+    extend: {},
+  },
+  plugins: [],
+}
