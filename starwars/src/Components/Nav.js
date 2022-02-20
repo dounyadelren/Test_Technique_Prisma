2b723bfb19f43320ac09776b9a4c5baa95718e2d
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-20 17:21:01 +0100

    [update] nav.js nav.css restructuration dû aux conflits sur les positions

--- starwars/src/Components/Nav.js
+++ starwars/src/Components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "../Assets/css/burger.css"
 import Logo from "../Assets/Assets/header.png"
+import '../Assets/css/nav.css'
 
 const Nav = () => {
     return (
         <>
-            <div className="relative">
-                <nav className="sm:hidden top-0 left-0 sticky z-10">
-                    <ul className="flex justify-evenly bg-transparent text-white sticky">
+            <div className="">
+                <nav className="nav sm:hidden">
+                    <ul className="">
                         <li>LE CÔTÉ LUMINEUX</li>
                         <li>LE CÔTÉ OBSCUR</li>
                         <li>L'EMPIRE</li>
@@ -16,13 +17,13 @@ const Nav = () => {
                         <li>JABA LE HUTT</li>
                     </ul>
                 </nav>
-                <img src={Logo} alt='logo' className="w-full absolute top-0 left-0" />
+                <img src={Logo} alt='logo' className="w-full top-0 left-0 mt-[-50px] img" />
                 {/* <div className="sm:h-50 sm:w-50 sm:bg-starwars sm:bg-center sm:bg-no-repeat sm:bg-cover"></div> */}
-                <nav className="hidden sm:block">
+                {/* <nav className="hidden sm:block">
                     <button className="h-8 w-8 navbar_burger z-10 right-0 absolute m-2">
                         <span className="burger-bar"></span>
                     </button>
-                </nav>
+                </nav> */}
             </div>
         </>
     )
