6c0b61e62bc6179332793183352881ba7c115ca1
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-20 17:20:02 +0100

    [add] article et article2 ds App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,11 +1,14 @@
 import './Assets/css/App.css';
 import Nav from "./Components/Nav"
 import "../src/Assets/css/globals.css"
-
+import Article from "../src/Components/Article"
+import Article2 from "../src/Components/Article2"
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Article />
+      <Article2 />
     </div>
   );
 }
