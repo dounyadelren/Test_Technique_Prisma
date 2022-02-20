6a40da5b29a9645d3d3acc7491d592dbec7f3773
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-20 17:19:09 +0100

    [update] tailwindconfig.js, modif du transparent et ajout fontsizes recup sur maquette

--- starwars/tailwind.config.js
+++ starwars/tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
   ],
   theme: {
     colors: {
-      transparent: "rgba(0,0,0,0)",
+      transparent: "rgba(0,0,0,0.7)",
       white: "#fff",
     },
     screens: {
       'sm': { 'max': '375px' },
       'lg': { 'min': '376px', 'max': '1024px' },
+    },
+    fontSize: {
+      sm: ['14px'],
+      md: ['18px'],
+      lg: ['32px'],
     },
     extend: {
       // backgroundImage: { 'starwars': "url('/src/Assets/Assets/header.png')", }
