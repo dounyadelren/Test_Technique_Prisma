f96ceef63c533da500309fe49131ad1cbc244f91
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-21 16:50:10 +0100

    [add] footer implémenté et intégré

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,7 @@ import Nav from "./Components/Nav"
 import "../src/Assets/css/globals.css"
 import Article from "../src/Components/Article"
 import Article2 from "../src/Components/Article2"
+import Footer from "../src/Components/Footer"
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,8 @@ function App() {
       <div className="h-[100px]"></div>
       <Article />
       <Article2 />
+      <div className="h-[61px]"></div>
+      <Footer />
     </div>
   );
 }
