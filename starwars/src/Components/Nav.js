4b267c953b145c42750f34f5f5b15157faf1cd09
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-21 15:21:21 +0100

    [update] nav.js nav.css, bg superposé en couches pour faciliter le futur breakpoint

--- starwars/src/Components/Nav.js
+++ starwars/src/Components/Nav.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import "../Assets/css/burger.css"
-import Logo from "../Assets/Assets/header.png"
+import Bg from "../Assets/Assets/header-bg-2x.png"
 import '../Assets/css/nav.css'
+import Logo from "../Assets/Assets/logo.png"
 
 const Nav = () => {
     return (
         <>
-            
-                <nav className="nav sm:hidden">
-                    <ul className="">
-                        <li>LE CÔTÉ LUMINEUX</li>
-                        <li>LE CÔTÉ OBSCUR</li>
-                        <li>L'EMPIRE</li>
-                        <li>LES DROÏDES</li>
-                        <li>LES EWOKS</li>
-                        <li>JABA LE HUTT</li>
-                    </ul>
-                </nav>
-                <img src={Logo} alt='logo' className="w-full top-0 left-0 mt-[-50px] img" />
-                {/* <div className="sm:h-50 sm:w-50 sm:bg-starwars sm:bg-center sm:bg-no-repeat sm:bg-cover"></div> */}
-                {/* <nav className="hidden sm:block">
+
+            <nav className="nav sm:hidden">
+                <ul className="">
+                    <li>LE CÔTÉ LUMINEUX</li>
+                    <li>LE CÔTÉ OBSCUR</li>
+                    <li>L'EMPIRE</li>
+                    <li>LES DROÏDES</li>
+                    <li>LES EWOKS</li>
+                    <li>JABA LE HUTT</li>
+                </ul>
+            </nav>
+            <div className="bg-starwars relative h-[500px] ">
+                <img src={Logo} alt="bg" className="img" />
+            </div>
+            {/* <img src={Bg} alt='logo' /> */}
+            {/* <div className="sm:h-50 sm:w-50 sm:bg-starwars sm:bg-center sm:bg-no-repeat sm:bg-cover"></div> */}
+            {/* <nav className="hidden sm:block">
                     <button className="h-8 w-8 navbar_burger z-10 right-0 absolute m-2">
                         <span className="burger-bar"></span>
                     </button>
                 </nav> */}
-            
+
         </>
     )
 }
