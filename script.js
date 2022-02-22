0e35fa2efdd485e3f99e890ac0ceabaf375d9167
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-02-22 15:23:37 +0100

    [update] script.js , add event.preventDefault pour eviter le rechargement de la page a chaque clic

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const main = document.getElementById('main')
 const navBurger = document.getElementById('nav_burger')
 const cross = document.getElementById('close')
 
-burger.addEventListener('click', function() {
-    // console.log("test")
+burger.addEventListener('click', function(event) {
+    event.preventDefault()
     main.style.display = 'none';
     navBurger.style.display='block';
 })
 
-cross.addEventListener('click', function() {
+cross.addEventListener('click', function(event) {
+    event.preventDefault()
     navBurger.style.display = 'none'
     main.style.display = 'block';
 })
